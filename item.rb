edc9d8afa22e539c372a17c8c3035c00427ea48b
mutai-gilbert <mutaigilbert85@gmail.com>
2023-05-30 12:36:44 +0400

    Add new files

--- item.rb
+++ item.rb
@@ -1,20 +1,46 @@
+require 'date'
+
 class Item
-  def initialize(genre, author, source, label, publish_date)
+  attr_reader :id, :genre, :author, :source, :label, :publish_date, :archived
+
+  def initialize(publish_date)
     @id = rand(1..9999)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
-    @publish_date = publish_date
+    @publish_date = parse_date(publish_date)
     @archived = false
   end
 
+  def author=(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+
   def can_be_archived?
-    return true if @publish_date > 10
+    Time.now.year - @publish_date.year > 10
   end
 
   def move_to_archive
     @archived = true if can_be_archived? == true
   end
-  private :can_be_archived?
+
+  def parse_date(date_str)
+    Date.parse(date_str)
+  rescue ArgumentError, TypeError
+    raise ArgumentError, 'Invalid publish date format'
+  end
+  private :can_be_archived?, :parse_date
 end
