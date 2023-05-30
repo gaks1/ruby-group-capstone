8e73d9936a85b06b14934f3ca563ecce1125a23a
Gmanlove <emmagmanc@gmail.com>
2023-05-30 15:06:48 +0100

    Add data.rb file

---
+++ data.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+def save_album(album)
+  JSON.dump(album, File.open('./storage/albums.json', 'w+'))
+end
+
+def save_genre(genre)
+  JSON.dump(genre, File.open('./storage/genre.json', 'w+'))
+end
+
+
+def save_data(item)
+  save_album(item.music_albums)
+  save_genre(item.genres)
+end
